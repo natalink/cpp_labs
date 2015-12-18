4111e4bf061cff46b694dc7716c41fd8dc17dcab
kljueva <natalink@seznam.cz>
2015-12-18 16:05:59 +0100

    new version

--- du/inf_system.cpp
+++ du/inf_system.cpp
@@ -1,14 +1,30 @@
+#include <iostream>
 #include "inf_system.h"
+#include <vector>       
+
+using namespace std;
 
 
-inf_system::inf_system()
-{
+vector<tuple> vector_of_tuples;
+
+void inf_system::insert(const string& first_name, const std::string& last_name, const position& position,
+const size_t& telephone, const std::string& address){
+    auto t = make_tuple(first_name, last_name, position, telephone, address);
+    cout << "Pridano: " << first_name << endl;
+  //  add_to_vector_of_tuples
+    vector_of_tuples.push_back(t);
+    
 }
 
-
 int main(){
-inf_system inf_system;
-inf_system.insert("Pepa", "Skoba", student, 2343241324, "adresa nejaka");
+    inf_system inf_system;
+    inf_system.insert("Pepa", "Skoba", student, 2343241324, "adresa nejaka");
 
-
+ 
+}  
+void print_vector(const vector<inf_system::row_def_const_ptr>& vector) {
+    for (const auto& r : vector) {
+        cout << get<0>(*r) << " " << get<1>(*r) << " " << get<2>(*r) <<
+        " " << get<3>(*r) << " " << get<4>(*r) << endl;
+    }
 }
--- du/inf_system.h
+++ du/inf_system.h
@@ -6,11 +6,15 @@ using namespace std;
 
 class inf_system
 {
+public:
 typedef tuple<string, string, position, size_t, string> row_def;
 typedef const row_def * row_def_const_ptr; 
 template<size_t idx>
 using type = typename tuple_element<idx, row_def>::type;
 
-void insert(const string& first_name, const std::string& last_name, const position& position, const size_t& telephone, const std::string& address);
-
+void insert(const string& first_name, const std::string& last_name, const position& position,
+const size_t& telephone, const std::string& address);
 };
+
+
+
